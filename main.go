756430de672866577b2dd8b42b8a66b9db712dab
Glycoene <qty2823650328@163.com>
2025-04-03 19:45:56 +0800

    v1.0版本

--- main.go
+++ main.go
@@ -1,10 +1,53 @@
-package Fools_Day_Tricky_Website
+package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"io/fs"
 	"net/http"
+	"path/filepath"
 )
 
-func main() {
+func getImages(dir string) ([]string, error) {
+	var images []string
 
+	err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if !d.IsDir() {
+			ext := filepath.Ext(path)
+			if ext == ".png" || ext == ".jpg" || ext == ".jpeg" || ext == ".gif" {
+				images = append(images, "/"+filepath.ToSlash(path))
+			}
+		}
+		return nil
+	})
+	return images, err
+}
+
+func main() {
+	router := gin.Default()
+
+	router.Static("/Img", "./Img")
+	router.LoadHTMLGlob("HTML/*")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	router.GET("/search", func(c *gin.Context) {
+		keywords := c.Query("q")
+		searchPath := "Img/" + keywords
+		images, err := getImages(searchPath)
+		if err != nil {
+			c.HTML(http.StatusOK, "search.html", gin.H{
+				"error": err,
+			})
+		}
+		c.HTML(http.StatusOK, "search.html", gin.H{
+			"Images": images,
+		})
+	})
+
+	_ = router.Run(":8080")
 }
